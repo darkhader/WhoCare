ebe77b7489c12590880547154f2f6f772ce92c53
unknown <dr.hiep98@gmail.com>
2020-01-19 15:58:34 +0700

    f

--- src/Components/NewGame.js
+++ src/Components/NewGame.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { Form, FormGroup, Input, Button, Table, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import UserImage from "./UserImage";
 import axios from "../axios";
 import { ROOT_API } from '../statics';
-import FacebookLogin from 'react-facebook-login';
+
 import Header from "./Header";
 import Loading from "./Loading";
 export default class NewGame extends Component {
@@ -12,57 +13,68 @@ export default class NewGame extends Component {
 		this.state = {
 			images: [],
 			userFB: "",
-			loading: false
+			loading: false,
+			userAlias: null,
+			userId: null
 		}
 
-		this.fbLogin = this.fbLogin.bind(this);
-		this.responseFacebook = this.responseFacebook.bind(this);
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 
 
 	}
 	componentDidMount() {
 
 	}
-	fbLogin = (scores) => { }
-	responseFacebook = (response) => {
+	handleInputChange(event) {
+
+		if (isNaN(event.target.value)) {
+
+
+			this.setState({ userAlias: event.target.value });
+		} else {
+			this.setState({ userId: event.target.value });
+		}
+	}
+	handleSubmit = (event) => {
+
+		event.preventDefault();
 
 		this.setState({
 			loading: true,
-			userFB: response
 		});
-		const userData = this.state.userFB ? {
-			facebook_alias: null,
-			facebook_id: this.state.userFB.id,
+		const userData = {
+			facebook_alias: this.state.userAlias,
+			facebook_id: this.state.userId,
 			deep_level: "fast"
-		} : "";
-		
-
-		if (userData) {
+		};
 			axios.post(`${ROOT_API}/api/anaRoute`, userData).then(response => {
 				if (response) {
+				
+					
 					if (response.data.success) {
 						this.setState({
 							images: JSON.parse(response.data.body),
 							loading: false
 						});
 					}
-					
+
 
 				}
 
 			}).catch(error => {
 
-				console.log(error)
+
 
 
 			});
-		}
+		
+
 
 	}
-
 	render() {
-		const displayedImages = this.state.images.slice(0, 12).map((user, index) => (
-			<div key={user.id} className="col-3 mb-4 ">
+		const displayedImages = this.state.images.slice(0, 9).map((user, index) => (
+			<div key={user.id} className="col-4 mb-4 ">
 
 				<UserImage
 					hiddenReview={true}
@@ -77,20 +89,29 @@ export default class NewGame extends Component {
 				{this.state.userFB ? <Header userFB={this.state.userFB} /> : <Header />}
 				{loading ? <div className="text-center"><Loading /></div>
 					:
-					<div className="container">
+					<div className=" d-flex justify-content-center">
+						<Form className=" d-flex flex-column align-items-start" onSubmit={this.handleSubmit}>
+							<h3 className="">Điền facebookID của bạn:</h3>
+							<span>VD:  https://facebook.com/hoanghiep, <br />
+								https://www.facebook.com/profile.php?id=123</span>
+							<div className="d-flex flex-row align-items-start mt-2 mr-2" >
+								<FormGroup className="mr-3">
+
+
+									<Input
+										placeholder="hoanghiep hoặc 123"
+										onChange={this.handleInputChange}
+
+									/>
+								</FormGroup>
+								<button	 className="btn btn-primary">Tìm Kiếm</button>
+							</div>
+						</Form>
 
 						
-
-						{this.state.userFB ? "" : <FacebookLogin
-							appId="477949126119051"
-							autoLoad={true}
-							fields="name,email,link"
-							onClick={this.fbLogin}
-							callback={this.responseFacebook} />}
-
-						<div className="row">{displayedImages}</div>
 					</div>
 				}
+				<div className="row">{displayedImages}</div>
 			</div>
 
 		);
--- src/Components/UserImage.js
+++ src/Components/UserImage.js
@@ -8,19 +8,18 @@ class UserImage extends Component {
         super(props);
 
         this.state = {
-            avatar: "67563683055",
+            avatar: "100002748713372",
         }
 
 
     }
     componentDidMount() {
         const userData =  {
-            facebook_alias: null,
-            facebook_id: this.props.user.id
+            facebook_alias: this.props.user.id,
+            facebook_id: null
         } ;
         axios.post(`${ROOT_API}/api/anaRoute`, userData).then(response => {
             if (response) {
-                console.log("response2 ", response);
                 this.setState({ avatar: response.data.avatar });
             }
         }).catch(error => {
@@ -34,12 +33,11 @@ class UserImage extends Component {
         return (
             <div className="box" >
                 <div className=" d-flex justify-content-center">
-                    {this.props.index < 5 ? <h3>Top {this.props.index}</h3> : <h3> </h3>}
+                    {this.props.index < 4 ? <h3>Top {this.props.index}</h3> : <h3> </h3>}
 
                 </div>
 
                 <img className="user_image"
-                    // style={{ width: "100%", height: "100%" }}
                     className=""
                     src={`http://graph.facebook.com/${this.state.avatar}/picture?type=large`}
                 />
